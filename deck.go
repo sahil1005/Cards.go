1ac5dfa32b1433dcb949a6f0bc1621dedd2b5b02
sahil1005 <sahilbhonde@outlook.com>
2022-05-15 21:20:11 +0530

    func added

--- deck.go
+++ deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 // create a new type of deck
 //which is a silce of strings
@@ -32,5 +37,30 @@ func (d deck) print()  {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+	
+}
+
+func newDeckFromFile(filename string) deck  {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// option 1- log the error and retrun a call to newdeck()
+		// option 2- log the error and entirely quit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")  
+	return deck(s)
+
 }
 
--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
+// import "fmt"
+
+
 func main() {
-	
-	cards := newDeck()
 
-	hand, reaminingCards := deal(cards, 5)
+	// cards := newDeck()
 
-	hand.print()
-	reaminingCards.print()
+	// hand, reaminingCards := deal(cards, 5)
+
+	// hand.print()
+	// reaminingCards.print()
+
+
+
+
+	// cards := newDeck()
+	//  // fmt.Println(cards.saveToFile())
+	// cards.saveToFile("My_cards")
+
+
+	cards := newDeckFromFile("My_cards")
+	cards.print()
+
 }
 
